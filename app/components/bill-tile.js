dd08702ed0c8e1bce49628670e9a4ffa1b4d5a3f
Adam Luchini and Jim Klein <adam+jim@>
2016-08-23 13:02:50 -0700

    added additional sorting with computed properties.

--- app/components/bill-tile.js
+++ app/components/bill-tile.js
@@ -4,8 +4,20 @@ export default Ember.Component.extend({
 
   dayToCompare: new Date("2016-06-23"),
 
-  billsFromPastMonth: Ember.computed.filter('bill', function(bill) {
-        return (new Date(bill.last_version_on) > this.dayToCompare);
-    })
+  sortBy: ['last_version_on:asc'],
+  sortedBills: Ember.computed.sort('bill', 'sortBy'),
+
+  billsFromPastMonth: Ember.computed.filter('sortedBills', function(bill) {
+    return (new Date(bill.last_version_on) > this.dayToCompare);
+  }),
+
+  billsWithShortNameWherePossible: Ember.computed.map('billsFromPastMonth', function(bill) {
+    if (bill.short_title != null){
+      bill.official_title = bill.short_title;//"bob";//bill.official_name;
+    }
+    //debugger;
+    return bill;
+  })
+
 
 });
